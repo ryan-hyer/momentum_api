868ff331e6645f9b8a178b3197a27eb7ab2b04f4
kimardenmiller <kimardenmiller@gmail.com>
2019-04-18 17:04:24 -0700

    - faster theme change code for groups

--- users/users_preferences - theme.rb
+++ users/users_preferences - theme.rb
@@ -1,6 +1,6 @@
 require '../utility/momentum_api'
 
-@do_live_updates = false
+@do_live_updates = true
 
 @instance = 'live' # 'live' or 'local'
 
@@ -21,14 +21,14 @@ require '../utility/momentum_api'
     theme_ids
 )
 
-@target_groups = %w(trust_level_0)  # LaunchpadV
+@target_groups = %w(MidPen)  # LaunchpadV, trust_level_0
 @exclude_user_names = %w(js_admin Winston_Churchill sl_admin JP_Admin admin_sscott RH_admin )
 @field_settings = "%-18s %-14s %-16s %-12s %-12s %-17s %-14s\n"
 
 @user_count, @user_targets, @users_updated = 0, 0, 0, 0
 
 def print_user_options(user_details)
-  printf @field_settings, @users_username,
+  printf @field_settings, user_details['username'],
          user_details[@user_option_print[0].to_s].to_s[0..9], user_details[@user_option_print[1].to_s].to_s[0..9],
          user_details[@user_option_print[2].to_s], user_details[@user_option_print[3].to_s],
          user_details[@user_option_print[4].to_s], user_details['user_option'][@user_option_print[5].to_s]
@@ -36,19 +36,20 @@ end
 
 # standardize_email_settings
 def apply_function(client, user)
-  @users_username = user['username']
+  users_username = user['username']
+  # puts users_username, client.api_username
   @user_count += 1
-  user_details = client.user(@users_username)
+  user_details = client.user(users_username)
   user_groups = user_details['groups']
   user_option = user_details['user_option']
 
   user_groups.each do |group|
-    @group_name = group['name']
-    if @issue_users.include?(@users_username)
-      puts "\n#{@users_username}  Group: #{@group_name}\n"
+    group_name = group['name']
+    if @issue_users.include?(users_username)
+      puts "\n#{users_username}  Group: #{group_name}\n"
     end
 
-    if @target_groups.include?(@group_name)
+    if @target_groups.include?(group_name)
       # what to update
       existing_value = user_option[@user_option_targets.keys[0].to_s]
       # printf "Existing value: %-20s \n", existing_value
@@ -57,19 +58,19 @@ def apply_function(client, user)
       value_already_correct = existing_value == target_value
       # all_settings_true = [user_option[@user_option_targets.keys[0].to_s]].all?
       if value_already_correct
-        puts 'User already correct'
-        print_user_options(user_details)
-      else
-        # puts 'User to be updated'
+        # puts 'User already correct'
         # print_user_options(user_details)
+      else
+        puts 'User to be updated'
+        print_user_options(user_details)
         @user_targets += 1
         if @do_live_updates
-          update_response = client.update_user(@users_username, @user_option_targets)
+          update_response = client.update_user(users_username, @user_option_targets)
           puts update_response[:body]['success']
           @users_updated += 1
 
           # check if update happened
-          user_details_after_update = client.user(@users_username)
+          user_details_after_update = client.user(users_username)
           print_user_options(user_details_after_update)
           sleep(1)
         end
@@ -83,6 +84,12 @@ printf @field_settings, 'UserName',
        @user_option_print[0], @user_option_print[1], @user_option_print[2],
        @user_option_print[3], @user_option_print[4], @user_option_print[5]
 
-apply_to_all_users
+if @target_groups
+  @target_groups.each do |group_plug|
+    apply_to_group_users(group_plug, needs_user_client=false, skip_staged_user=true)
+  end
+else
+  apply_to_all_users
+end
 
 puts "\n#{@users_updated} users updated out of #{@user_targets} possible targets out of #{@user_count} total users."
