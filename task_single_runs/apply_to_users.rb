2ab5b1ddbb507f730412cba63b958350808f5e53
kimardenmiller <kimardenmiller@gmail.com>
2019-06-02 16:58:00 -0700

    - Hash move to single run

--- task_single_runs/apply_to_users.rb
+++ task_single_runs/apply_to_users.rb
@@ -2,13 +2,18 @@ require '../lib/momentum_api'
 
 do_live_updates           =   false
 
-scan_options = {
+scan_options = {                        # todo move to using the Class from this point forward.
     team_category_watching:   true,
     essential_watching:       true,
     growth_first_post:        true,
     meta_first_post:          true,
     trust_level_updates:      true,
-    score_user_levels:        'not_voted',   # have_voted, not_voted, newly_voted, all
+    score_user_levels: {
+        update_type:  'not_voted',      # have_voted, not_voted, newly_voted, all
+        target_post:  28707,            # 28649
+        target_polls: %w(version_two),  # basic new version_two
+        poll_url:     'https://discourse.gomomentum.org/t/user-persona-survey/6485/20'
+    },
     user_group_alias_notify:  true
 }
 
