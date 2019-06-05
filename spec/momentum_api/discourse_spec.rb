4a574f999a4e16b16494f5761146689ea615f083
kimardenmiller <kimardenmiller@gmail.com>
2019-06-04 23:23:25 -0700

    - Discourse Class 81% covered

--- spec/momentum_api/discourse_spec.rb
+++ spec/momentum_api/discourse_spec.rb
@@ -3,9 +3,7 @@ require_relative '../spec_helper'
 describe MomentumApi::Discourse do
 
   do_live_updates =   false
-  target_username =   nil
-  target_groups   =   %w(trust_level_1) # OwnerExpired Mods GreatX BraveHearts trust_level_1 trust_level_0
-  
+
   scan_options = {
       team_category_watching:   true,
       essential_watching:       true,
@@ -25,7 +23,8 @@ describe MomentumApi::Discourse do
   let(:user_details) { json_fixture("user.json") }
   let(:category_list) { json_fixture("categories.json") }
 
-  describe ".apply_to_users several users" do
+  
+  describe '.apply_to_users - all' do
 
     let(:mock_dependencies) do
       mock_dependencies = instance_double('mock_dependencies')
@@ -37,16 +36,37 @@ describe MomentumApi::Discourse do
     end
 
     subject { MomentumApi::Discourse.new('KM_Admin', 'live', do_live_updates = do_live_updates,
-                                         target_groups=target_groups, target_username=target_username, mock: mock_dependencies) }
+                                         target_groups=nil, target_username=nil, mock: mock_dependencies) }
+
+    it 'responds to apply_to_users and runs thru default group of users' do
+      subject.apply_to_users(scan_options)
+      expect(subject).to respond_to(:apply_to_users)
+    end
+  end
+
+  
+  describe '.apply_to_users in group' do
+
+    let(:mock_dependencies) do
+      mock_dependencies = instance_double('mock_dependencies')
+      expect(mock_dependencies).to receive(:group_members).and_return(group_member_list)
+      expect(mock_dependencies).to receive(:user).and_return(user_details).exactly(group_member_list.length).times
+      expect(mock_dependencies).to receive(:categories).and_return(category_list).exactly(group_member_list.length).times
+      expect(mock_dependencies).to receive(:run_scans).exactly(group_member_list.length).times
+      mock_dependencies
+    end
+
+    subject { MomentumApi::Discourse.new('KM_Admin', 'live', do_live_updates = do_live_updates,
+                                         target_groups=%w(trust_level_1), target_username=nil, mock: mock_dependencies) }
 
     it 'responds to apply_to_users and runs thru group of users' do
       subject.apply_to_users(scan_options)
       expect(subject).to respond_to(:apply_to_users)
     end
-
   end
 
-  describe ".apply_to_users single user" do
+
+  describe '.apply_to_users single user' do
 
     let(:mock_dependencies) do
       mock_dependencies = instance_double('mock_dependencies')
@@ -59,14 +79,14 @@ describe MomentumApi::Discourse do
 
     # target_username =   'test_user'
     subject { MomentumApi::Discourse.new('KM_Admin', 'live', do_live_updates = do_live_updates,
-                                         target_groups=target_groups, target_username='Tony_Christopher', mock: mock_dependencies) }
+                                         target_groups=%w(trust_level_1), target_username='Tony_Christopher', mock: mock_dependencies) }
 
     it 'responds to apply_to_users and runs thru single user' do
       subject.apply_to_users(scan_options)
       expect(subject).to respond_to(:apply_to_users)
     end
-
   end
+
 
   describe "client.group_members may raise DiscourseApi::TooManyRequests error" do
     
@@ -77,14 +97,14 @@ describe MomentumApi::Discourse do
     end
 
     subject { MomentumApi::Discourse.new('KM_Admin', 'live', do_live_updates = do_live_updates,
-                                         target_groups=target_groups, target_username=target_username, mock: mock_dependencies) }
+                                         target_groups=%w(trust_level_1), target_username=nil, mock: mock_dependencies) }
 
     it "responds to unknown .connect_to_instance" do
       expect { subject.apply_to_users(scan_options) }
           .to raise_error('DiscourseApi::TooManyRequests')
     end
-
   end
+
 
   describe "client.user may raise DiscourseApi::TooManyRequests error" do
 
@@ -93,20 +113,61 @@ describe MomentumApi::Discourse do
       expect(mock_dependencies).to receive(:group_members).and_return(group_member_list).once
       expect(mock_dependencies).to receive(:user).exactly(2).times
                                        .and_raise(DiscourseApi::TooManyRequests.new('error message here'))
-      # expect(mock_dependencies).to receive(:categories).and_return(category_list).exactly(group_member_list.length).times
-      # expect(mock_dependencies).to receive(:run_scans).exactly(group_member_list.length).times
       mock_dependencies
     end
 
     subject { MomentumApi::Discourse.new('KM_Admin', 'live', do_live_updates = do_live_updates,
-                                         target_groups=target_groups, target_username=target_username, mock: mock_dependencies) }
+                                         target_groups=%w(trust_level_1), target_username=nil, mock: mock_dependencies) }
     
     it "responds to unknown .connect_to_instance" do
       expect { subject.apply_to_users(scan_options) }
         .to raise_error(DiscourseApi::TooManyRequests)
     end
-
   end
+
+
+  describe "client.categories may raise DiscourseApi::TooManyRequests" do
+
+    let(:mock_dependencies) do
+      mock_dependencies = instance_double('mock_dependencies')
+      expect(mock_dependencies).to receive(:group_members).and_return(group_member_list).once
+      expect(mock_dependencies).to receive(:user).and_return(user_details).once
+      expect(mock_dependencies).to receive(:categories).exactly(2).times
+                                       .and_raise(DiscourseApi::TooManyRequests.new('error message here'))
+      mock_dependencies
+    end
+
+    subject { MomentumApi::Discourse.new('KM_Admin', 'live', do_live_updates = do_live_updates,
+                                         target_groups=%w(trust_level_1), target_username=nil, mock: mock_dependencies) }
+
+    it "responds to unknown .connect_to_instance" do
+      expect { subject.apply_to_users(scan_options) }
+        .to raise_error(DiscourseApi::TooManyRequests)
+    end
+  end
+
+
+  describe "client.categories may raise DiscourseApi::UnauthenticatedError" do
+
+    let(:mock_dependencies) do
+      mock_dependencies = instance_double('mock_dependencies')
+      expect(mock_dependencies).to receive(:group_members).and_return(group_member_list).once
+      expect(mock_dependencies).to receive(:user).and_return(user_details).exactly(group_member_list.length).times
+      expect(mock_dependencies).to receive(:categories).exactly(group_member_list.length).times
+                                       .and_raise(DiscourseApi::UnauthenticatedError.new(' - Not permitted to view resource.'))
+      expect(mock_dependencies).to receive(:run_scans).exactly(group_member_list.length).times
+      mock_dependencies
+    end
+
+    subject { MomentumApi::Discourse.new('KM_Admin', 'live', do_live_updates = do_live_updates,
+                                         target_groups=%w(trust_level_1), target_username=nil, mock: mock_dependencies) }
+
+    it "responds to unknown .connect_to_instance" do
+      subject.apply_to_users(scan_options)
+      expect(subject).to respond_to(:apply_to_users)
+    end
+  end
+
 
   describe ".connect_to_instance" do
 
@@ -116,7 +177,7 @@ describe MomentumApi::Discourse do
     end
 
     subject { MomentumApi::Discourse.new('KM_Admin', 'live', do_live_updates = do_live_updates,
-                                         target_groups=target_groups, target_username=target_username, mock: mock_dependencies) }
+                                         target_groups=%w(trust_level_1), target_username=nil, mock: mock_dependencies) }
 
     it "responds to live .connect_to_instance" do
       subject.connect_to_instance('KM_Admin', 'live')
