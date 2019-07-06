603b7a4372f493298ab9da0da9f402f6195b9b0e
kimardenmiller <kimardenmiller@gmail.com>
2019-07-05 19:20:09 -0700

    - homepage_id working

---
+++ _task_runs/homepage.rb
@@ -0,0 +1,29 @@
+require_relative 'log/utility'
+require '../lib/momentum_api'
+
+discourse_options = {
+    do_live_updates:        true,
+    target_username:        'Kim_Miller',     # David_Kirk Steve_Scott Marty_Fauth Kim_Miller David_Ashby
+    target_groups:          %w(trust_level_1),       # Mods GreatX BraveHearts trust_level_1
+    instance:               'live',
+    api_username:           'KM_Admin',
+    exclude_users:           %w(js_admin Winston_Churchill sl_admin JP_Admin admin_sscott RH_admin KM_Admin),
+    issue_users:             %w(),
+    logger:                  momentum_api_logger
+}
+
+schedule_options = {
+    user:{
+        preferences:                              {
+            homepage_id: {
+                do_task_update:         false ,
+                allowed_levels:         2,
+                set_level:              2,
+                excludes:               %w()
+            }
+        }
+    }
+}
+discourse = MomentumApi::Discourse.new(discourse_options, schedule_options)
+discourse.apply_to_users
+discourse.scan_summary
