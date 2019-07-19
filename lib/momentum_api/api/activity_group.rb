d79aa2c86e3ba41c0c61d70e1926fee844b40a28
kimardenmiller <kimardenmiller@gmail.com>
2019-07-19 14:16:58 -0700

    - Activity Groupping confirmed working

--- lib/momentum_api/api/activity_group.rb
+++ lib/momentum_api/api/activity_group.rb
@@ -40,6 +40,8 @@ module MomentumApi
 
       if @options       # Active Users (60 * 60) = 1 hour   recent_time_read = last 60 days
 
+        user_label = nil
+
         read_post_ratio = nil
         if man.user_details['post_count'] > 0
           read_post_ratio = (man.user_details['time_read'] / 60) / man.user_details['post_count']
@@ -49,7 +51,8 @@ module MomentumApi
             (man.user_details['time_read'] > 10 * (60 * 60) or man.user_details['recent_time_read'] > 1 * (60 * 60))
 
           if @options[:active_user][:do_task_update]
-            man.print_user_options(man.user_details, user_label: 'Active Momentum User  ', hash: {'Read-Post Ratio': read_post_ratio})
+            user_label = 'Active Momentum User  '
+            # man.print_user_options(man.user_details, user_label: user_label, hash: {'Read-Post Ratio': read_post_ratio})
             target_activity_groups            = [@options[:active_user][:set_level]]
             counters[:'Active User Count']    += 1
           end
@@ -58,7 +61,8 @@ module MomentumApi
             (man.user_details['time_read'] > 1 * (60 * 60) or man.user_details['recent_time_read'] > 0.2 * (60 * 60))
 
           if @options[:average_user][:do_task_update]
-            man.print_user_options(man.user_details, user_label: 'Average Momentum User ', hash: {'Read-Post Ratio': read_post_ratio})
+            user_label = 'Average Momentum User '
+            # man.print_user_options(man.user_details, user_label: user_label, hash: {'Read-Post Ratio': read_post_ratio})
             target_activity_groups            = [@options[:active_user][:set_level]]
             counters[:'Average User Count']   += 1
           end
@@ -66,7 +70,8 @@ module MomentumApi
         elsif @options[:email_user] and man.user_details['post_count'] > 5
 
           if @options[:email_user][:do_task_update]
-            man.print_user_options(man.user_details, user_label: 'Email Momentum User   ', hash: {'Read-Post Ratio': read_post_ratio})
+            user_label = 'Email Momentum User   '
+            # man.print_user_options(man.user_details, user_label: user_label, hash: {'Read-Post Ratio': read_post_ratio})
             target_activity_groups            = [@options[:email_user][:set_level]]
             counters[:'Email User Count']     += 1
           end
@@ -74,41 +79,45 @@ module MomentumApi
         elsif @options[:inactive_user]
 
           if @options[:inactive_user][:do_task_update]
-            man.print_user_options(man.user_details, user_label: 'Inactive Momentum User', hash: {'Read-Post Ratio': read_post_ratio})
+            user_label = 'Inactive Momentum User'
+            # man.print_user_options(man.user_details, user_label: user_label, hash: {'Read-Post Ratio': read_post_ratio})
             target_activity_groups            = [@options[:inactive_user][:set_level]]
             counters[:'Inactive User Count']  += 1
           end
 
         end
-      end
 
-      if @schedule.discourse.options[:do_live_updates] and target_activity_groups and
-          target_activity_groups != mans_current_activity_groups
+        if @schedule.discourse.options[:do_live_updates] and target_activity_groups and
+            target_activity_groups != mans_current_activity_groups
 
-        if mans_current_activity_groups.empty?
-          puts 'Man in none of these groups'
-        else
-          mans_current_activity_groups.each do |current_group_id|
-            remove_response = @schedule.discourse.admin_client.group_remove(current_group_id, username: man.user_details['username'])
-            @mock ? sleep(0) : sleep(1)
-            man.discourse.options[:logger].warn "Removed man: #{remove_response.body['success']}"
-            @counters[:'User Removed from Group'] += 1
+          man.print_user_options(man.user_details, user_label: user_label, hash: {'Read-Post Ratio': read_post_ratio})
+
+          if mans_current_activity_groups.empty?
+            # puts 'Man in none of these groups'
+          else
+            mans_current_activity_groups.each do |current_group_id|
+              remove_response = @schedule.discourse.admin_client.group_remove(current_group_id, username: man.user_details['username'])
+              @mock ? sleep(0) : sleep(1)
+              man.discourse.options[:logger].warn "Removed man from Group #{current_group_id}: #{remove_response.body['success']}"
+              @counters[:'User Removed from Group'] += 1
+            end
+          end
+
+          update_response = @schedule.discourse.admin_client.group_add(target_activity_groups[0], username: man.user_details['username'])
+          @mock ? sleep(0) : sleep(1)
+          man.discourse.options[:logger].warn "Added man to Group #{target_activity_groups[0]}: #{update_response.body['success']}"
+          @counters[:'User Group Updated'] += 1
+
+          # check if update happened ... or ... comment out for no check after update
+          user_details_after_update = @schedule.discourse.admin_client.user(man.user_details['username'])
+          @mock ? sleep(0) : sleep(1)
+          user_details_after_update['groups'].each do |user_group_after_update|
+            if user_group_after_update['id'] == target_activity_groups[0]
+              man.discourse.options[:logger].warn "#{user_details_after_update['username']} added to #{user_group_after_update['name']}"
+            end
           end
         end
 
-        update_response = @schedule.discourse.admin_client.group_add(target_activity_groups[0], username: man.user_details['username'])
-        @mock ? sleep(0) : sleep(1)
-        man.discourse.options[:logger].warn update_response.body['success']
-        @counters[:'User Group Updated'] += 1
-
-        # check if update happened ... or ... comment out for no check after update
-        user_details_after_update = @schedule.discourse.admin_client.user(man.user_details['username'])
-        @mock ? sleep(0) : sleep(1)
-        user_details_after_update['groups'].each do |user_group_after_update|
-          if user_group_after_update['id'] == target_activity_groups[0]
-            man.discourse.options[:logger].warn "#{user_details_after_update['username']} added to #{user_group_after_update['name']}"
-          end
-        end
       end
 
       counters[:time_read]          += man.user_details['time_read']
